a3a2fa02a5b5061fc12de651690a4a534c2d3953
Nausheen-star <nausheenbano825@gmail.com>
2026-06-19 11:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (83.82%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,9 +1,25 @@
+// class Solution {
+//     public int getSum(int a, int b) {
+        
+//         if(b==0){
+//             return a;
+//         }
+//         return getSum(a^b, (a&b) << 1);
+//     }
+// }
+
 class Solution {
     public int getSum(int a, int b) {
-        
-        if(b==0){
-            return a;
+
+        while (b != 0) {
+
+            int carry = (a & b) << 1;
+
+            a = a ^ b;
+
+            b = carry;
         }
-        return getSum(a^b, (a&b) << 1);
+
+        return a;
     }
 }

4d8306b0f5f69540d6260c02043df0d088085fde
Nausheen-star <nausheenbano825@gmail.com>
2026-06-24 10:28:06 +0530

    Implement recursive approach for zigzag arrays
    Added commented-out recursive approach with memoization for zigzag arrays.

--- 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
@@ -50,3 +50,57 @@ class Solution {
         return (int) ans;
     }
 }
+
+
+
+//Approach-1 (Recursion + Memoization) - TLE
+//T.C : O(n*m*m), we have n*m*2 states and we run for loop inside the recursion
+//S.C : O(n*m)
+// class Solution {
+//     int MOD = 1_000_000_007;
+//     int N, M;
+//     int[][][] t;
+
+//     public int zigZagArrays(int n, int l, int r) {
+//         N = n;
+//         M = r - l + 1;
+//         t = new int[N + 1][M + 1][2];
+//         for (int[][] a : t)
+//             for (int[] b : a)
+//                 Arrays.fill(b, -1);
+
+//         long result = 0;
+//         for (int startVal = 1; startVal <= M; startVal++) {
+//             //a < b > c < d ...
+//             result = (result + solve(1, startVal, true)) % MOD;
+//             //a > b < c > d...
+//             result = (result + solve(1, startVal, false)) % MOD;
+//         }
+//         return (int) result;
+//     }
+
+//     private int solve(int i, int prevVal, boolean increasing) {
+//         if (i == N) { //able to find N elements of zigzag array
+//             return 1;
+//         }
+
+//         int dir = increasing ? 1 : 0;
+//         if (t[i][prevVal][dir] != -1) {
+//             return t[i][prevVal][dir];
+//         }
+
+//         int result = 0;
+//         if (increasing) {
+//             for (int nextVal = prevVal + 1; nextVal <= M; nextVal++) {
+//                 result = (result + solve(i + 1, nextVal, false)) % MOD;
+//             }
+//         } else {
+//             for (int nextVal = 1; nextVal < prevVal; nextVal++) {
+//                 result = (result + solve(i + 1, nextVal, true)) % MOD;
+//             }
+//         }
+
+//         return t[i][prevVal][dir] = result;
+//     }
+// }
+

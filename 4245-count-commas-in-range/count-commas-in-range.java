32311c1c3f1a056f28220b0157c6866c668e6c1f
Nausheen-star <nausheenbano825@gmail.com>
2026-09-08 21:40:29 +0530

    Time: 8 ms (8.47%) | Memory: 42.8 MB (8.32%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,24 @@
+//Approach-1 (Simple simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int countCommas(int n) {
+        int result = 0;
+        for (int num = 1; num <= n; num++) {
+            if (num > 999) {
+                result += 1;
+            }
+        }
+
+        return result;
+    }
+}
+
+// //Approach-2 (Simple observation)
+// //T.C : O(1)
+// //S.C : O(1)
+// class Solution {
+//     public int countCommas(int n) {
+//         return Math.max(n - 999, 0);
+//     }
+// }

97d576baa1fcc22f2902e3c69a00589c934355bd
Nausheen-star <nausheenbano825@gmail.com>
2026-09-20 11:42:37 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (81.16%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Reverse alphabet value
+            int value = 'z' - ch + 1;
+
+            // Position is i + 1
+            sum += value * (i + 1);
+        }
+
+        return sum;
+    }
+}
